5db72fd146fe84a099f5c9ee9476d24d327c3d12
Ranieri Oliveira <114862419+ranierioliveira@users.noreply.github.com>
2023-06-06 10:57:24 -0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const form = document.querySelector('form#novoItem');
+
+form.addEventListener('submit', (evento) => {
+    evento.preventDefault(); //bloqueia o evento padrão 
+    console.log(evento);
+
+    console.log(evento.target.elements['nome'].value);
+    console.log(evento.target.elements['quantidade'].value)
+})
